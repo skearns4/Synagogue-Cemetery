50c5e7ebfd337fd2e896bbcf76cfedc6faf8dffd
brittanyregrut <BNR12@pitt.edu>
2015-11-16 00:15:06 -0500

    Display table now has checkboxes instead of buttons. Can select multiple entries to view. Delete ButtonEditor.java and ButtonRenderer.java -- no longer necessary

--- src/DisplayPanel.java
+++ src/DisplayPanel.java
@@ -1,27 +1,85 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.*;
 
 /**
  * DisplayPanel class designed to output database queries in MainWindow
  */
 public class DisplayPanel extends JPanel
 {
-  private Object[][] data = new Object[100][7];//data for the search result table
-
+  //Model for the table of search results
+  private DefaultTableModel model;
   /**
    * DisplayPanel constructor
    */
   public DisplayPanel()
   {
-    String[] columns = new String[]{//headers for the search result table
-        "First Name", "Last Name", "Section", "Plot Number", "Grave Number", "Date Deceased", "View Entry"
+    model = new DefaultTableModel() {
+      //Switch statement specifying first 6 rows as strings, and the last row to be booleans (checkboxes)
+      public Class<?> getColumnClass(int column) {
+        switch (column) {
+          case 0:
+            return String.class;
+          case 1:
+            return String.class;
+          case 2:
+            return String.class;
+          case 3:
+            return String.class;
+          case 4:
+            return String.class;
+          case 5:
+            return String.class;
+          case 6:
+            return Boolean.class;
+          default:
+            return String.class;
+        }
+      }
     };
-    JTable searchTable = new JTable(data, columns); //create search result table
-    searchTable.getColumn("View Entry").setCellRenderer(new ButtonRenderer());
-    searchTable.getColumn("View Entry").setCellEditor(new ButtonEditor(new JCheckBox()));
-    setLayout(new GridLayout(1, 1));
-    add(new JScrollPane(searchTable)); //add table to the display panel
+
+    //Initialize table and set model
+    JTable searchTable = new JTable();
+    searchTable.setModel(model);
+
+    //Add columns with appropriate headings to the table
+    model.addColumn("First Name");
+    model.addColumn("Last Name");
+    model.addColumn("Section");
+    model.addColumn("Plot Number");
+    model.addColumn("Grave Number");
+    model.addColumn("Date Deceased");
+    model.addColumn("Select Entry");
+
+    //Add the table to the display panel
+    setLayout(new GridLayout(2, 1));
+    add(new JScrollPane(searchTable));
+
+    //Create a new button and add to the display panel
+    JButton select = new JButton("View Entries");
+    add(select);
     setVisible(true);
+
+    //Action listener for the select button to view checked search results
+    select.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+
+        //determine if a row is selected
+        for (int i = 0; i < searchTable.getRowCount(); i++) {
+          Boolean checked = Boolean.valueOf(searchTable.getValueAt(i, 6).toString());
+          String col = searchTable.getValueAt(i, 1).toString();
+
+          //display an entry view/edit box for each selected entry
+          if (checked) {
+            JOptionPane.showMessageDialog(null, col);
+          }
+        }
+      }
+    });
   }
 
   /**
@@ -34,17 +92,18 @@ public class DisplayPanel extends JPanel
    * @param pn  plot number
    * @param gn  grave number
    * @param d   date
-   * @param num row the result is to be displayed on
+   * @param num number of rows
    */
   public void add(String fn, String ln, String sn, String pn, String gn, String d, int num)
   {
-    data[num][0] = fn;
-    data[num][1] = ln;
-    data[num][2] = sn;
-    data[num][3] = pn;
-    data[num][4] = gn;
-    data[num][5] = d;
-    data[num][6] = "Select";
+    model.addRow(new Object[0]);
+    model.setValueAt(fn, num, 0);
+    model.setValueAt(ln, num, 1);
+    model.setValueAt(sn, num, 2);
+    model.setValueAt(pn, num, 3);
+    model.setValueAt(gn, num, 4);
+    model.setValueAt(d, num, 5);
+    model.setValueAt(false, num, 6);
     updateUI();
   }
 
@@ -53,8 +112,9 @@ public class DisplayPanel extends JPanel
    */
   public void clear()
   {
-    for (int i = 0; i < 100; i++)
-      for (int j = 0; j < 7; j++)
-        this.data[i][j] = "";
+    int numRows = model.getRowCount();
+    for (int i = 0; i < numRows; i++){
+      model.removeRow(i);
+    }
   }
 }
